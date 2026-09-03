e8c451f6cdb69bd7b619981b76ca2fdc2f710e69
MariaMorales1234 <mjmorales@uniboyaca.edu.co>
2026-09-02 23:22:45 -0500

    logica de la vista catalogo

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -1,22 +1,29 @@
 // Script base para la vista de catálogo
 // Aquí deben consumir la API de items y mostrarlos en la página
+import { getItems, getItem, createItem, updateItem, deleteItem } from "./services/api.js";
+import { renderItems, resetForm, fillForm } from "./ui/ui.js";
 
 // Constante con la URL base de la API
 const API_URL = "/api/items";
-
-// TODO: Seleccionar el contenedor donde se mostrarán los items
-// const catalogContainer = document.getElementById("...");
+const catalogContainer = document.getElementById("catalogContainer");
 
 // Función principal para cargar los items desde la API
 async function loadCatalog() {
     try {
         // 1. Hacer fetch a la API (GET /api/items)
+        const res = await fetch(API_URL);
         // 2. Parsear la respuesta a JSON
+        const items = await res.json();
         // 3. Limpiar el contenedor del catálogo
+        catalogContainer.innerHTML = "";
         // 4. Iterar sobre cada item y llamar a renderItem()
+        items.forEach(item => {
+            renderItem(item);
+        });
     } catch (err) {
         console.error("Error cargando catálogo:", err);
         // TODO: Mostrar mensaje de error en la UI
+        alert("No se pudo cargar los items");
     }
 }                                                   
 
@@ -25,6 +32,17 @@ function renderItem(item) {
     // TODO: Crear un elemento HTML (ej: div o card)
     // TODO: Asignar los datos del item (name, description, etc.)
     // TODO: Insertar el elemento en el contenedor
+    const row = document.createElement("div");
+    row.innerHTML = `
+        <div class = "card">
+            <div class = "card-container">
+                <h1 class = "name">${item.name}</h1>
+                <p class = "description">${item.description}</p>
+                <p class = "price">${item.price};
+            </div>
+        </div>
+    `;
+    catalogContainer.appendChild(row)
 }
 
 // Inicializar el catálogo cuando cargue la página
